8cc656c0ee6ef366c95aa536b90c87e8f61fabbe
Ruboi98 <155378641+Ruboi98@users.noreply.github.com>
2024-03-26 16:59:30 +0200

    Add project files.

---
+++ MortgageCalculator/MortgageCalculator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class MortgageCalculator
+{
+    public double CalculateMonthlyRepayment(double loanAmount, double annualInterestRate, int loanTermYears)
+    {
+        double monthlyInterestRate = annualInterestRate / 12 / 100;
+        int totalPayments = loanTermYears * 12;
+        double monthlyRepayment = loanAmount * (monthlyInterestRate * Math.Pow(1 + monthlyInterestRate, totalPayments)) /
+                                   (Math.Pow(1 + monthlyInterestRate, totalPayments) - 1);
+        return monthlyRepayment;
+    }
+
+    public double CalculateTotalInterestPaid(double loanAmount, double annualInterestRate, int loanTermYears)
+    {
+        double monthlyRepayment = CalculateMonthlyRepayment(loanAmount, annualInterestRate, loanTermYears);
+        int totalPayments = loanTermYears * 12;
+        double totalAmountPaid = monthlyRepayment * totalPayments;
+        double totalInterestPaid = totalAmountPaid - loanAmount;
+        return totalInterestPaid;
+    }
+
+    public double CalculateTotalAmountPaid(double loanAmount, double annualInterestRate, int loanTermYears)
+    {
+        double monthlyRepayment = CalculateMonthlyRepayment(loanAmount, annualInterestRate, loanTermYears);
+        int totalPayments = loanTermYears * 12;
+        return monthlyRepayment * totalPayments;
+    }
+
+    public List<AmortizationScheduleEntry> GenerateAmortizationSchedule(double loanAmount, double annualInterestRate, int loanTermYears)
+    {
+        List<AmortizationScheduleEntry> schedule = new List<AmortizationScheduleEntry>();
+        double monthlyRepayment = CalculateMonthlyRepayment(loanAmount, annualInterestRate, loanTermYears);
+        double monthlyInterestRate = annualInterestRate / 12 / 100;
+        int totalPayments = loanTermYears * 12;
+        double remainingBalance = loanAmount;
+
+        for (int i = 1; i <= totalPayments; i++)
+        {
+            double interestPaid = remainingBalance * monthlyInterestRate;
+            double principalPaid = monthlyRepayment - interestPaid;
+            remainingBalance -= principalPaid;
+            schedule.Add(new AmortizationScheduleEntry(i, monthlyRepayment, interestPaid, principalPaid, remainingBalance));
+        }
+
+        return schedule;
+    }
+}
+
+public class AmortizationScheduleEntry
+{
+    public int PaymentNumber { get; set; }
+    public double PaymentAmount { get; set; }
+    public double InterestPaid { get; set; }
+    public double PrincipalPaid { get; set; }
+    public double RemainingBalance { get; set; }
+
+    public AmortizationScheduleEntry(int paymentNumber, double paymentAmount, double interestPaid, double principalPaid, double remainingBalance)
+    {
+        PaymentNumber = paymentNumber;
+        PaymentAmount = paymentAmount;
+        InterestPaid = interestPaid;
+        PrincipalPaid = principalPaid;
+        RemainingBalance = remainingBalance;
+    }
+}
---
+++ MortgageCalculator/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+     class Program
+    {
+        static void Main(String[]args)
+        {
+            // Welcome message
+            Console.WriteLine("Welcome to UXI Mortgage Loan By Nhlovo Mdaka");
+
+            // Get user input
+            Console.Write("Enter the loan amount: ");
+            double loanAmount = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the annual interest rate (in percentage): ");
+            double annualInterestRate = double.Parse(Console.ReadLine());
+
+            Console.Write("Enter the loan term in years: ");
+            int loanTermYears = int.Parse(Console.ReadLine());
+
+            // Create MortgageCalculator object
+            MortgageCalculator calculator = new MortgageCalculator();
+
+            // Calculate and display results
+            double monthlyRepayment = calculator.CalculateMonthlyRepayment(loanAmount, annualInterestRate, loanTermYears);
+            double totalInterestPaid = calculator.CalculateTotalInterestPaid(loanAmount, annualInterestRate, loanTermYears);
+            double totalAmountPaid = calculator.CalculateTotalAmountPaid(loanAmount, annualInterestRate, loanTermYears);
+
+            // Print mortgage calculation results
+            Console.WriteLine("\nMortgage Calculation Results:");
+            // Print monthly repayment amount
+            Console.WriteLine("Monthly Repayment: R" + monthlyRepayment.ToString("F2"));
+            // Print total interest paid
+            Console.WriteLine("Total Interest Paid: R" + totalInterestPaid.ToString("F2"));
+            // Print total amount paid
+            Console.WriteLine("Total Amount Paid: R" + totalAmountPaid.ToString("F2"));
+
+
+        // Generate and display amortization schedule
+        Console.WriteLine("\nAmortization Schedule:");
+            Console.WriteLine("Payment No.\tPayment Amount\tInterest Paid\tPrincipal Paid\tRemaining Balance");
+            var amortizationSchedule = calculator.GenerateAmortizationSchedule(loanAmount, annualInterestRate, loanTermYears);
+            foreach (var entry in amortizationSchedule)
+            {
+               
+                Console.WriteLine(entry.PaymentNumber + "\t\t" + entry.PaymentAmount.ToString("F2") + "\t\t" + entry.InterestPaid.ToString("F2") + "\t\t" + entry.PrincipalPaid.ToString("F2") + "\t\t" + entry.RemainingBalance.ToString("F2"));
+
+        }
+    }
+    }
+
